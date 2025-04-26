eb595ce22d3ebb8200ffc4332a701f1bfc3cc18f
yared bekuru <69724291+yaredybk@users.noreply.github.com>
2025-04-26 15:55:04 +0300

    restart to install updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('node:path');
 
 const ROOT = path.resolve(process.env.ROOT || '../');
 console.log(new Date());
-
+let foundUpdate = false;
 async function pullFromOrigin(repos = []) {
   for (const repo of repos) {
     const repoPath = path.join(ROOT, repo);
@@ -18,8 +18,16 @@ async function pullFromOrigin(repos = []) {
         exec('git pull --force', { cwd: repoPath }, (error, stdout, stderr) => {
           if (error) {
             console.error(`Error pulling ${repo}: ${error.message}`);
-          } 
+          }
           console.log(`STDOUT ${repo}: ${stdout}`);
+          if (
+            stdout.includes('Updating') &&
+            !stdout.includes('Already up to date') &&
+            !foundUpdate
+          ) {
+            foundUpdate = true;
+            console.log(`**** Found update in ${repo} ****`);
+          }
           resolve(stdout);
         });
       });
@@ -36,10 +44,40 @@ const repos = ['garage_v5'];
 if (process.env.NODE_ENV !== 'development') {
   repos.push('proxy_local', 'print_server', 'updater');
 }
-(
-  async () => {
-    await pullFromOrigin(repos);
-    console.log('**** Pulling from origin completed ****');
-    process.exit();
+(async () => {
+  await pullFromOrigin(repos);
+  console.log('**** Pulling from origin completed ****');
+  if (foundUpdate) {
+    console.log('**** Found update ****');
+    let headers3 = {
+      __from: 'proxy_local',
+    };
+    let baseurl = process.env.LOCAL_URL || 'http://127.0.0.1:10001';
+    let url = `${baseurl}/api/dev/update`;
+    Promise.all([
+      fetch(url, {
+        method: 'POST',
+        headers: headers3,
+        body: JSON.stringify({ update: true }),
+      }),
+      fetch(`http://127.0.0.1:10002/api/dev/update`, {
+        method: 'POST',
+        headers: headers3,
+        body: JSON.stringify({ update: true }),
+      }),
+    ])
+      .then((response) => {
+        if (response[0].ok) {
+          console.log('**** Update notification sent ****');
+        } else {
+          console.error('**** Failed to send update notification ****');
+        }
+      })
+      .catch((error) => {
+        console.error('**** Error sending update notification ****', error);
+      });
+  } else {
+    console.log('**** No updates found ****');
   }
-)()
+  process.exit();
+})();
