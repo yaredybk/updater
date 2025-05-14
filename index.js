910083f650a9afa291c1d0f4f3e0d1c9fdaf6e8f
yared b2 <yb12ybk@gmail.com>
2025-05-14 22:08:10 +0300

    http server

--- index.js
+++ index.js
@@ -1,10 +1,122 @@
-const { exec } = require('node:child_process');
-const { existsSync } = require('node:fs');
-const path = require('node:path');
+const { exec } = require("node:child_process");
+const { existsSync } = require("node:fs");
+const { createServer } = require("node:http");
+const path = require("node:path");
+let updating = false;
+const ROOT = path.resolve(process.env.ROOT || "../");
+// console.log(new Date());
 
-const ROOT = path.resolve(process.env.ROOT || '../');
-console.log(new Date());
-let foundUpdate = false;
+const repos = ["garage_v5"];
+if (process.env.NODE_ENV !== "development") {
+  repos.push("proxy_local", "print_server", "updater");
+}
+// checkForUpdates();
+async function checkForUpdates() {
+  if (updating) return Promise.resolve("Already updating");
+  updating = true;
+  let foundUpdate = false;
+  return pullFromOrigin(repos).then(() => {
+    console.log("**** Pulling from origin completed ****");
+    if (foundUpdate) {
+      console.log("**** Found update ****");
+      let headers3 = {
+        __from: "proxy_local",
+      };
+      let baseurl = process.env.LOCAL_URL || "http://127.0.0.1:10001";
+      let url = `${baseurl}/api/dev/update`;
+      return Promise.all([
+        fetch(url, {
+          method: "POST",
+          headers: headers3,
+          body: JSON.stringify({ update: true }),
+        }),
+        fetch(`http://127.0.0.1:10002/api/dev/update`, {
+          method: "POST",
+          headers: headers3,
+          body: JSON.stringify({ update: true }),
+        }),
+      ])
+        .then((response) => {
+          if (response[0].ok) {
+            console.log("**** Update notification sent ****");
+          } else {
+            console.error("**** Failed to send update notification ****");
+          }
+        })
+        .catch((error) => {
+          console.error("**** Error sending update notification ****", error);
+        })
+        .finally(() => {
+          updating = false;
+        });
+    } else {
+      console.log("**** No updates found ****");
+      updating = false;
+      return Promise.resolve("No updates found");
+    }
+  });
+}
+
+http
+  .createServer((req, res) => {
+    let body = [];
+    req
+      .on("data", (chunk) => body.push(chunk))
+      .on("end", () => {
+        body = Buffer.concat(body);
+        // extract json load
+        try {
+          body = JSON.parse(body.toString() || "{}");
+        } catch (error) {
+          console.warn(error);
+        } finally {
+          listnner(req, res, body);
+        }
+      });
+  })
+  .listen(process.env.UPDATE_PORT || 10004, () => {
+    console.log(
+      "update server running at http://localhost:" +
+        (process.env.UPDATE_PORT || 10004)
+    );
+  });
+
+async function listnner(req, res, body) {
+  let url = req.url;
+  switch (url) {
+    case "/restart_computer":
+      fun = Promise.resolve("restarting computer");
+      exec(
+        'shutdown /r /t 10 /c "Restarting in 10 seconds for updates"',
+        (error, stdout, stderr) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+          }
+          console.log(`stdout: ${stdout}`);
+          console.error(`stderr: ${stderr}`);
+        }
+      );
+      break;
+    case "/update":
+      // fun = Promise.resolve("updating");
+      checkForUpdates()
+        .then(() => {
+          res.writeHead(200, "done");
+          res.end("done");
+        })
+        .catch((error) => {
+          console.error(error);
+          res.writeHead(500, "failed");
+          res.end(error);
+        });
+      break;
+
+    default:
+      break;
+  }
+}
+
 async function pullFromOrigin(repos = []) {
   for (const repo of repos) {
     const repoPath = path.join(ROOT, repo);
@@ -15,14 +127,14 @@ async function pullFromOrigin(repos = []) {
     console.log(`**** Pulling ${repo} ****`);
     try {
       await new Promise((resolve, reject) => {
-        exec('git pull --force', { cwd: repoPath }, (error, stdout, stderr) => {
+        exec("git pull --force", { cwd: repoPath }, (error, stdout, stderr) => {
           if (error) {
             console.error(`Error pulling ${repo}: ${error.message}`);
           }
           console.log(`STDOUT ${repo}: ${stdout}`);
           if (
-            stdout.includes('Updating') &&
-            !stdout.includes('Already up to date') &&
+            stdout.includes("Updating") &&
+            !stdout.includes("Already up to date") &&
             !foundUpdate
           ) {
             foundUpdate = true;
@@ -40,44 +152,4 @@ async function pullFromOrigin(repos = []) {
   }
 }
 
-const repos = ['garage_v5'];
-if (process.env.NODE_ENV !== 'development') {
-  repos.push('proxy_local', 'print_server', 'updater');
-}
-(async () => {
-  await pullFromOrigin(repos);
-  console.log('**** Pulling from origin completed ****');
-  if (foundUpdate) {
-    console.log('**** Found update ****');
-    let headers3 = {
-      __from: 'proxy_local',
-    };
-    let baseurl = process.env.LOCAL_URL || 'http://127.0.0.1:10001';
-    let url = `${baseurl}/api/dev/update`;
-    Promise.all([
-      fetch(url, {
-        method: 'POST',
-        headers: headers3,
-        body: JSON.stringify({ update: true }),
-      }),
-      fetch(`http://127.0.0.1:10002/api/dev/update`, {
-        method: 'POST',
-        headers: headers3,
-        body: JSON.stringify({ update: true }),
-      }),
-    ])
-      .then((response) => {
-        if (response[0].ok) {
-          console.log('**** Update notification sent ****');
-        } else {
-          console.error('**** Failed to send update notification ****');
-        }
-      })
-      .catch((error) => {
-        console.error('**** Error sending update notification ****', error);
-      });
-  } else {
-    console.log('**** No updates found ****');
-  }
-  process.exit();
-})();
+setInterval(() => checkForUpdates(), 1000 * 3600 * 3); // every 3 hours
